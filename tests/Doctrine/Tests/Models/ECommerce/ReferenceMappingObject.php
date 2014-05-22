4e91da1be9447895af8b93293bd64295749f2554
ElectricMaxxx <Maximilian.Berghoff@gmx.de>
2014-05-22 07:52:19 +0200

    fix travis setting

--- tests/Doctrine/Tests/Models/ECommerce/ReferenceMappingObject.php
+++ tests/Doctrine/Tests/Models/ECommerce/ReferenceMappingObject.php
@@ -4,13 +4,25 @@
 namespace Doctrine\Tests\ORM\ODMAdapter\Mapping\Driver\Model;
 
 use Doctrine\ORM\ODMAdapter\Mapping\Annotations as ODMAdapter;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * A class with no explicitly set properties for testing default values.
+ *
  * @ODMAdapter\ObjectAdapter
+ * @ORM\Entity(table="orm_odm_tests")
  */
 class ReferenceMappingObject
 {
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     public $uuid;
 
     /**
@@ -26,5 +38,8 @@ class ReferenceMappingObject
      */
     public $referencedField;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     public $entityName;
 }
