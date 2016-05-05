3db77ae712339580a5d20c220305bb61593613d7
Damian <bigfootdd@gmail.com>
2016-05-05 18:59:37 +0200

    Updated vendor requirements

--- tests/travis_doctrine_dbal.sh
+++ tests/travis_doctrine_dbal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 composer require jackalope/jackalope-doctrine-dbal:~1.0 --no-update
-composer update --prefer-source
+composer update --prefer-stable
 
 SCRIPT_NAME="${0##*/}"
 SCRIPT_DIR="${0%/*}"
@@ -19,5 +19,5 @@ fi
 mysql -e 'create database IF NOT EXISTS orm_odm_tests;' -u root
 
 cp ${SCRIPT_DIR}/../cli-config.doctrine_dbal.php.dist ${SCRIPT_DIR}/../cli-config.php
-${SCRIPT_DIR}/../bin/phpcrodm jackalope:init:dbal
+${SCRIPT_DIR}/../bin/phpcrodm jackalope:init:dbal --force
 ${SCRIPT_DIR}/../bin/phpcrodm doctrine:phpcr:register-system-node-types
